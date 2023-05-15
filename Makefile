e4c81232b64349fb6940de6574da1481cb64d0a1
minatore06 <stei2002@hotmail.it>
2023-05-15 14:42:29 +0200

    added struct t_program

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ NAME = connect4.a
 
 OUT = connect4
 
-SRCS = 
+SRCS = main.c
 
 OBJS = ${SRCS:.c=.o}
 
--- connect4.h
+++ connect4.h
@@ -1,4 +1,13 @@
 #ifndef CONNECT4_H
 # define CONNECT4_H
 # include "libft/libft.h"
+
+typedef struct s_program
+{
+    int height;
+    int width;
+    int **matrix;
+    int win;
+}   t_program;
+
 #endif
