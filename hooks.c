2b047383e39c801bac7714af8bf33eb4c9208f9d
jacopo3101 <107195063+jacopo3101@users.noreply.github.com>
2023-05-17 12:32:46 +0200

    commit prova

--- hooks.c
+++ hooks.c
@@ -3,25 +3,35 @@
 /*                                                        :::      ::::::::   */
 /*   hooks.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kristori <kristori@student.42.fr>          +#+  +:+       +#+        */
+/*   By: javellis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/16 15:41:35 by javellis          #+#    #+#             */
-/*   Updated: 2023/05/17 10:02:34 by kristori         ###   ########.fr       */
+/*   Updated: 2023/05/17 11:51:01 by javellis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 
 #include"connect4.h"
 #include<stdio.h>
 
 int ft_input(int button, int x, int y, t_program *param)
 {
-    (void)param;
     t_program *prog = (t_program *)param;
     printf("x = %d, y = %d, button = %d\n", x, y, button);
+    ft_put(prog, x / 64);
+        mlx_clear_window(prog->mlx, prog->window);
+        ft_draw_grid(prog);
+        printf("ok\n");
+        ft_printf("\e[1;1H\e[2J");
+        ft_draw_grid_terminal(prog);
+        prog->player = 1;
+    if(check_win(*prog, x / 64, prog->player) == prog->player)
+    {
+        printf("hai vinto!\n");
+        return 0;
+    }
+    // ai_plays(*prog, prog->player);
     // ft_draw(prog);
     // mlx_put_image_to_window(prog->mlx, prog->window, prog->buffer.reference, 0, 0);
-    ft_draw_grid(prog);
-	ft_printf("\e[1;1H\e[2J");
-	ft_draw_grid_terminal(prog);
     return 1;
 }
--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kristori <kristori@student.42.fr>          +#+  +:+       +#+        */
+/*   By: javellis <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 14:49:29 by ncortigi          #+#    #+#             */
-/*   Updated: 2023/05/17 10:11:40 by kristori         ###   ########.fr       */
+/*   Updated: 2023/05/17 12:02:32 by javellis         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,54 @@ int	main(int argc, char **argv)
 
 	if (!check_input(argc, argv))
 		return (0);
+// ------------
+// 	take_input(&data, argv);
+
+//  	ft_gui_init(&data);
+// 	ft_printf("ok\n");
+// 	ft_draw_grid(&data);
+// 	mlx_mouse_hook(data.window, ft_input, &data);
+// 	mlx_hook(data.window, 17, 0, ft_close, &data);
+// 	mlx_loop(data.mlx);
+
+	// ft_printf("\e[1;1H\e[2J");
+	// ft_draw_start_terminal(&data);
+	// if (get_next_line(0))
+	// 	ft_printf("\e[1;1H\e[2J");
+	// ft_random_start(&data);
+	// if (data.player == 2)
+	// {
+	// 	data.matrix[data.height - 1][data.width / 2] = 2;
+	// }
+	// while (1)
+	// {
+	// 	ft_draw_grid_terminal(&data);
+	// 	ft_printf("Insert n of column: ");
+	// 	input = get_next_line(0);
+	// 	if (ft_check_input_string(input))
+	// 	{
+	// 		column = ft_atoi(input);
+	// 		ft_printf("\e[1;1H\e[2J");
+	// 		ft_put(&data, column - 1);
+	// 		ai_plays(data, data.player);
+	// 	}
+	// 	else
+	// 	{
+	// 		ft_printf("\e[1;1H\e[2J");
+	// 		ft_printf("Invalid number, retry\n");
+	// 	}
+	// }
+// ---------
 	take_input(&data, argv, argc);
 	if (data.gui > 0)
 	{
+		ft_random_start(&data);
+		if (data.player == 2)
+		{
+			data.matrix[data.height - 1][data.width / 2] = 2;
+		}
 		ft_gui_init(&data);
+	    ft_draw_grid(&data);
 		mlx_mouse_hook(data.window, ft_input, &data);
 		mlx_hook(data.window, 17, 0, ft_close, &data);
 		mlx_loop(data.mlx);
@@ -49,7 +93,7 @@ int	main(int argc, char **argv)
 				column = ft_atoi(input);
 				ft_printf("\e[1;1H\e[2J");
 				ft_put(&data, column - 1);
-				ai_plays(data, data.player);
+				// ai_plays(data, data.player);
 			}
 			else
 			{
